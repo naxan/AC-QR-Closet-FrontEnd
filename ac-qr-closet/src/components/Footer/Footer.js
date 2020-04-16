aea5f4fcb65eb099285056dc83b6f4fe42484c13
akempel <anna.kb.kempel@gmail.com>
2020-04-15 20:50:37 -0700

    Replaces test heading in footer with basic footer design

--- ac-qr-closet/src/components/Footer/Footer.js
+++ ac-qr-closet/src/components/Footer/Footer.js
@@ -1,8 +1,18 @@
 import React from "react";
+import { Icon, Menu } from "semantic-ui-react";
+import "./Footer.css";
 
 class Footer extends React.Component {
   render() {
-    return <h1>Footer</h1>;
+    return (
+      <div className="Footer">
+        <p>Thanks for visiting!</p>
+        <p>This website was made by Anna Kempel</p>
+        <Menu.Item href="https://github.com/naxan" target="_blank">
+          <Icon id="github-icon" name="github" />
+        </Menu.Item>
+      </div>
+    );
   }
 }
 
