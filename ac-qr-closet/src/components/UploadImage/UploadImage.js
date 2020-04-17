85ec872b8f75b6a72ff76b77a5dbdccb7bb75538
akempel <anna.kb.kempel@gmail.com>
2020-04-16 20:54:44 -0700

    Adds pick file and upload buttons to image upload. Displays image before choosing to upload

--- ac-qr-closet/src/components/UploadImage/UploadImage.js
+++ ac-qr-closet/src/components/UploadImage/UploadImage.js
@@ -1,8 +1,12 @@
 import React from "react";
+import { Button } from "semantic-ui-react";
+import "./UploadImage.css";
 
 class UploadImage extends React.Component {
   state = {
     uploadedImage: null,
+    uploadedImageURL: null,
+    uploadStatus: null,
   };
 
   setDefaultImage() {
@@ -11,31 +15,49 @@ class UploadImage extends React.Component {
     });
   }
 
-  uploadImage(e) {
+  handleFileSelect = (e) => {
+    this.setState({
+      uploadedImage: e.target.files[0],
+      uploadedImageURL: URL.createObjectURL(e.target.files[0]),
+    });
+  };
+
+  uploadImage = () => {
     let imageFormObj = new FormData();
     imageFormObj.append("imageName", "image-" + Date.now());
-    imageFormObj.append("imageData", e.target.files[0]);
-
-    this.setState({
-      uploadedImage: URL.createObjectURL(e.target.files[0]),
-    });
+    imageFormObj.append("imageData", this.state.uploadedImage);
 
     fetch("http://localhost:4000/image/upload", {
       method: "POST",
       body: imageFormObj,
     }).then((data) => {
       if (data.ok) {
-        alert("Image has been successfully uploaded!");
-        this.setDefaultImage();
+        this.setState({
+          uploadStatus: "Image has been successfully uploaded!",
+        });
       }
     });
-  }
+  };
 
   render() {
     return (
       <div className="process">
-        <input type="file" onChange={(e) => this.uploadImage(e)} />
-        <img src={this.state.uploadedImage} alt="upload-image" />
+        <input
+          style={{ display: "none" }}
+          type="file"
+          onChange={(e) => this.handleFileSelect(e)}
+          ref={(fileInput) => (this.fileInput = fileInput)}
+        />
+        <Button onClick={() => this.fileInput.click()}>Pick File</Button>
+        <Button onClick={this.uploadImage}>Upload</Button>
+        <h3>{this.state.uploadStatus}</h3>
+        {this.state.uploadedImageURL && (
+          <img
+            className="uploaded-image"
+            src={this.state.uploadedImageURL}
+            alt="upload-image"
+          />
+        )}
       </div>
     );
   }
