cedc1138f55accdd189c9e81debfb9ab468a748e
akempel <anna.kb.kempel@gmail.com>
2020-04-19 17:33:42 -0700

    Adjusts footer spacing

--- ac-qr-closet/src/components/Footer/Footer.js
+++ ac-qr-closet/src/components/Footer/Footer.js
@@ -6,23 +6,25 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="Footer">
-        <p>Thanks for visiting!</p>
-        <p>
-          Background image supplied by{" "}
-          <a href="https://catwithmonocle.com/">Cat With Monocle</a>. Several
-          vector graphics supplied by{" "}
-          <a href="https://www.vecteezy.com/">Vecteezy</a>.
-        </p>
-        <p>
-          Visit me on Github!{" "}
-          <Menu.Item
-            href="https://github.com/naxan"
-            target="_blank"
-            style={{ display: "inline" }}
-          >
-            <Icon id="github-icon" name="github" />
-          </Menu.Item>
-        </p>
+        <div className="content-container">
+          <p>Thanks for visiting!</p>
+          <p>
+            Background image supplied by{" "}
+            <a href="https://catwithmonocle.com/">Cat With Monocle</a>. Several
+            vector graphics supplied by{" "}
+            <a href="https://www.vecteezy.com/">Vecteezy</a>.
+          </p>
+          <p>
+            Visit me on Github!{" "}
+            <Menu.Item
+              href="https://github.com/naxan"
+              target="_blank"
+              style={{ display: "inline" }}
+            >
+              <Icon id="github-icon" name="github" />
+            </Menu.Item>
+          </p>
+        </div>
       </div>
     );
   }
